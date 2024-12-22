36b3041543c8e42bd4cfdf4d2860b2005cb5e7f0
FNewell <m0nst3rmc@gmail.com>
2024-12-22 11:44:37 +0100

    Updated mod initializer

--- src/main/java/github/fnewell/playerstatistics/PlayerStatistics.java
+++ src/main/java/github/fnewell/playerstatistics/PlayerStatistics.java
@@ -1,10 +1,10 @@
 package github.fnewell.playerstatistics;
 
-import com.mojang.brigadier.Command;
-import github.fnewell.playerstatistics.localdatabase.LocalDatabase;
+import github.fnewell.playerstatistics.db.DriverUtils;
 import github.fnewell.playerstatistics.utils.CommandUtils;
 import github.fnewell.playerstatistics.utils.ConfigUtils;
 import github.fnewell.playerstatistics.utils.StatSyncScheduler;
+import github.fnewell.playerstatistics.utils.StatSyncTask;
 import github.fnewell.playerstatistics.webserver.WebServer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
@@ -12,32 +12,52 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO: Add functionality to single player worlds - not priority
 public class PlayerStatistics implements ModInitializer {
 	public static final String MOD_ID = "player-statistics";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static boolean DEBUG = false;
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Loading Player Statistics ...");
+
 		// Initialize the config
-		ConfigUtils.initializeConfig();
+		if(!ConfigUtils.initializeConfig()) {
+			LOGGER.error("Failed to initialize config. Mod is not loaded!");
+			return;
+		}
+
+		// DEBUG: Check if debug mode is enabled
+		if (DEBUG) { LOGGER.info("Debug mode is enabled!"); }
+
+		// TODO: Download only the required drivers
+		// TODO: Add support for MariaDB/Mysql and PostgreSQL (not local SQLite too)
+		// Check for required drivers and download them if missing
+		DriverUtils.checkDrivers();
+
+		// Register the custom DB driver
+		DriverUtils.registerSQLite();
 
 		// Register commands
+		if (DEBUG) { LOGGER.info("Registering commands ..."); }
 		CommandUtils.registerCommands();
+		if (DEBUG) { LOGGER.info("Commands registered!"); }
 
 		// Run planned synchronization task
-		StatSyncScheduler.startScheduledSync();
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> StatSyncScheduler.startScheduledSync());
 
 		// Run web server if enabled in the config
-		/*if(ConfigUtils.config.getBoolean("web-server.enabled"))
-			WebServer.startServer();*/
+		if(ConfigUtils.config.getBoolean("web-server.enabled")) {
+			if (DEBUG) { LOGGER.info("Starting web server ..."); }
+			WebServer.startServer();
+			if (DEBUG) { LOGGER.info("Web server started!"); }
+		}
 
 		// Stop the scheduled synchronization task on server shutdown
 		ServerLifecycleEvents.SERVER_STOPPING.register(server -> StatSyncScheduler.stopScheduledSync());
+		ServerLifecycleEvents.SERVER_STOPPED.register(server -> StatSyncScheduler.stopScheduledSync());
 
-		LOGGER.info("PlayerStatistics loaded!");
+		LOGGER.info("Player Statistics successfully loaded!");
 	}
 }
